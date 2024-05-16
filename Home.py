9ae3052ba3c16468b77be99a1e74fa88c03ec5a7
Rosa Wu <u10050070@gmail.com>
2024-05-16 13:52:46 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -60,8 +60,6 @@ m.to_streamlit(height=320)
 
 
 # Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
 st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
 # And the root-level secrets are also accessible as environment variables:
