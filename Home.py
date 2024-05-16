758dcfc7325fc5aa12223a9c410f521e5bde675d
Rosa Wu <u10050070@gmail.com>
2024-05-16 20:30:03 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,11 +7,15 @@ from pymongo.server_api import ServerApi
 st.set_page_config(layout="wide")
 
 uri = "mongodb+srv://{st.secrets['db_username']}:{st.secrets['db_pswd']}@cluster0.zskuvse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+@st.experimental_singleton(suppress_st_warning=True)
 def init_connection():
     mClient = MongoClient(uri, server_api=ServerApi('1'))
     return mClient
         
 client = init_connection()
+
+
 @st.experimental_memo(ttl=60)
 def get_data():
     db = client.get_database('testdb')
