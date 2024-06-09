a5e2dd27526a212095327c94f539e434af4e17ec
Rosa Wu <u10050070@gmail.com>
2024-06-09 22:20:31 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -82,13 +82,11 @@ if st.button('添加心情'):
         ```json
         {
           "comfort_and_encouragement": "你的安慰和鼓勵文字",
-          "travel_recommendation": {
-            "city_name": "推薦的旅遊城市",
-            "country_name": "這個城市的國家",
-            "latitude": "這個城市的經度",
-            "longitude": "這個城市的緯度",
-            "reason": "這個城市的推薦原因"
-          }
+          "city_name": "推薦的旅遊城市",
+          "country_name": "這個城市的國家",
+          "latitude": "這個城市的經度",
+          "longitude": "這個城市的緯度",
+          "reason": "這個城市的推薦原因"
         }```
   '''
   result = get_completion([ {"role": "user", "content": prompt }], model="gpt-3.5-turbo")
