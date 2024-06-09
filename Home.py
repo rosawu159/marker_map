3ec301655b9af65277a9a1082683a8be6632a057
Rosa Wu <u10050070@gmail.com>
2024-06-09 21:50:30 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,6 +6,15 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 st.set_page_config(layout="wide")
 
+def get_completion(messages, model="gpt-3.5-turbo", temperature=0, max_tokens=1000):
+  payload = { "model": model, "temperature": temperature, "messages": messages, "max_tokens": max_tokens }
+  headers = { "Authorization": f'Bearer {API_KEY}', "Content-Type": "application/json" }
+  response = requests.post('https://api.openai.com/v1/chat/completions', headers = headers, data = json.dumps(payload) )
+  obj = json.loads(response.text)
+  if response.status_code == 200 :
+    return obj["choices"][0]["message"]["content"]
+  else :
+    return obj["error"]
 
 def init_connection():
     connection_string = "mongodb+srv://"+st.secrets['db_username']+":"+st.secrets['db_pswd']+"@cluster0.zskuvse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -74,3 +83,20 @@ m.to_streamlit(height=320)
 items = get_data()
 for i in items:
     st.info(i.get('latitude'))
+    st.info(i)
+
+prompt= f'''
+        請根據以下的日記內容，生成一段具有安慰和鼓勵性的文字，並提供一個適合的旅遊推薦。
+
+        日記內容：
+        {mood}
+
+        請生成的文字包含以下要素：
+        1. 安慰和鼓勵：對日記中表達的負面情緒進行安慰和鼓勵。
+        2. 旅遊推薦：根據日記中提到的需要放鬆，提供一個合適的旅遊城市推薦，並給我城市的latitude。
+        
+        生成的文字示例：
+        "我能感受到你這幾天承受了很多壓力。工作上的忙碌和疲憊有時候真的會讓人感到難以承受。但是請相信，這一切都會過去，你是如此堅強和有能力的人。給自己一點時間和空間，好好照顧自己。我建議你去[旅遊地點]旅行，那裡的[特點]非常適合放鬆和恢復精力。希望這次旅行能夠給你帶來平靜和快樂。加油，你一定可以的！"
+        
+        現在請根據上述要求，生成相應的文字。謝謝！'''
+result = get_completion([ {"role": "user", "content": prompt }], model="gpt-3.5-turbo")
