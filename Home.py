1ffedbc0ebc8d2d2ef6ea5c9aff14665d7267969
Rosa Wu <u10050070@gmail.com>
2024-06-13 05:04:49 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -23,7 +23,7 @@ def create_postcard(attraction_name):
         t_url = 'https://api.unsplash.com/search/photos?page=1&query=Parc-Guell&client_id=' + st.secrets['client_id']
         response = requests.get(t_url)
         json_data = response.json()
-        st.info(json_data)
+        st.info(json_data['results'][0]['urls']['raw'])
         if response.status_code == 200:
             image = Image.open(BytesIO(response.content))
             getpic = True
